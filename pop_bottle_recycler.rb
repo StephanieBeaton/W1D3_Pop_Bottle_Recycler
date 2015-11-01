1728e39b5ad4a67798c4c94c7979ffe7838d1ca4
Stephanie Beaton <stefanybeaton@gmail.com>
2015-10-31 20:14:22 -0700

    tidied up the output so that is prints all stats

--- pop_bottle_recycler.rb
+++ pop_bottle_recycler.rb
@@ -17,6 +17,8 @@
 # That gives a supply of both bottles and bottle caps that can be used to redeem
 #  for further bottles, which will then produce a further supply for recycling.
 
+# attempt at recursive solution that is not correct
+
 def total_bottles(original_quantity_purchased)
 
   return 1 if original_quantity_purchased <= 1
@@ -33,13 +35,49 @@ def total_bottles(original_quantity_purchased)
 
 end
 
-original_bottles = 10
+# original_bottles = 10
 
 # puts "original bottles #{original_bottles} total bottles #{total_bottles(original_bottles)}"
 
 # ===============================================================
 
 def iterative_total_bottles(original_bottles_purchased)
+
+  # definitions
+  #
+  # a "round" is when customer returns to the store with empty bottles and caps
+  #      to purchase full bottles
+  #
+  # total_bottles_bought - the total number bottles bought in all rounds when
+  #      the customer returns all bottles to buy more full bottles
+  #      and then returns those bottles, and so on until he/she runs out of bottles to return.
+
+  # total_bottles_bought_thru_bottle_recycling
+  #
+  # total_bottles_bought_thru_cap_recycling
+  #
+  # nbr_bottles_returned -  equals the nbr full bottles bought with empty bottles and caps in the previoues round
+  #       It is the sum of nbr_bottles_redeemed_from_bottles plus nbr_bottles_redeemed_from_caps in previous round
+  #
+  # remaining_caps - caps not redeemed for bottles in the current round, must be 3 or fewer
+  #
+  # remaining_bottles - botles not redeemed for bottles in the current round, most be 1 or 0
+  #
+  # total_caps - is the sum of the "remaining_caps" not redeemed in the previous round
+  #       and the the caps on the botttles returned in this round
+  #
+  # total_bottles - the sum of the "remaining bottles" which were not redeemed in the previous round
+  #       and the bottles returned in this round
+  #
+  # nbr_bottles_redeemed_from_bottles - is number bottles redeemed from total_bottles
+  #
+  # nbr_bottles_redeemed_from_caps - is number of bottles redeemed from total_caps
+  #
+  # old_nbr_bottles_returned - is just a variable for printing purposes
+  #
+  #  old_total_caps  - is just a variable for printing purposes
+  #
+
 
   total_bottles_bought = original_bottles_purchased
   nbr_bottles_returned = original_bottles_purchased
@@ -48,47 +86,76 @@ def iterative_total_bottles(original_bottles_purchased)
   remaining_bottles = 0
 
   remaining_caps = 0
+  total_bottles_bought_thru_bottle_recycling = 0
+  total_bottles_bought_thru_cap_recycling = 0
 
   while (nbr_bottles_returned + remaining_bottles) >= 2 || (nbr_bottles_returned + remaining_caps) >= 4 do
 
+    puts "nbr_bottles_returned #{nbr_bottles_returned}"
+    puts "remaining_bottles #{remaining_bottles}"
+    puts "nbr_bottles_returned + remaining_bottles #{nbr_bottles_returned + remaining_bottles}"
+    puts ""
+    puts "nbr_bottles_returned #{nbr_bottles_returned}"
+    puts "remaining_caps #{remaining_caps}"
+    puts "nbr_bottles_returned + remaining_caps #{nbr_bottles_returned + remaining_caps}"
+    puts ""
+
     total_caps = nbr_bottles_returned + remaining_caps
+    puts "total_caps = nbr_bottles_returned + remaining_caps"
+    puts "#{total_caps} = #{nbr_bottles_returned} + #{remaining_caps}"
 
     total_bottles = nbr_bottles_returned + remaining_bottles
+    puts "total_bottles = nbr_bottles_returned + remaining_bottles"
+    puts "#{total_bottles} = #{nbr_bottles_returned} + #{remaining_bottles}"
 
 
     nbr_bottles_redeemed_from_bottles = (total_bottles / 2).floor
+    puts "nbr_bottles_redeemed_from_bottles = (total_bottles / 2).floor"
+    puts "#{nbr_bottles_redeemed_from_bottles} = (#{total_bottles} / 2).floor"
+
+    total_bottles_bought_thru_bottle_recycling += nbr_bottles_redeemed_from_bottles
+    puts "so far total_bottles_bought_thru_bottle_recycling += nbr_bottles_redeemed_from_bottles"
+    puts "#{total_bottles_bought_thru_bottle_recycling}"
 
     remaining_bottles = total_bottles - (nbr_bottles_redeemed_from_bottles * 2)
+    puts "remaining_bottles = total_bottles - (nbr_bottles_redeemed_from_bottles * 2)"
+    puts "#{remaining_bottles} = #{total_bottles} - (#{nbr_bottles_redeemed_from_bottles} * 2)"
+
 
     nbr_bottles_redeemed_from_caps = (total_caps / 4).floor
+    puts "nbr_bottles_redeemed_from_caps = (total_caps / 4).floor"
+    puts "#{nbr_bottles_redeemed_from_caps} = (#{total_caps} / 4).floor"
+
+    total_bottles_bought_thru_cap_recycling += nbr_bottles_redeemed_from_caps
+    puts "so far total_bottles_bought_thru_cap_recycling += nbr_bottles_redeemed_from_caps"
+    puts "#{total_bottles_bought_thru_cap_recycling}"
 
     remaining_caps = total_caps - (nbr_bottles_redeemed_from_caps * 4)
+    puts "remaining_caps = total_caps - (nbr_bottles_redeemed_from_caps * 4)"
+    puts "#{remaining_caps} = #{total_caps} - (#{nbr_bottles_redeemed_from_caps} * 4)"
 
-    old_nbr_bottles_returned = nbr_bottles_returned
+    #old_nbr_bottles_returned = nbr_bottles_returned
     nbr_bottles_returned = nbr_bottles_redeemed_from_bottles + nbr_bottles_redeemed_from_caps
+    puts "future nbr_bottles_returned = nbr_bottles_redeemed_from_bottles + nbr_bottles_redeemed_from_caps"
+    puts "#{nbr_bottles_returned} = #{nbr_bottles_redeemed_from_bottles} + #{nbr_bottles_redeemed_from_caps}"
 
+    old_total_bottles_bought = total_bottles_bought
     total_bottles_bought += nbr_bottles_returned
+    puts "total_bottles_bought = total_bottles_bought + nbr_bottles_returned"
+    puts "#{total_bottles_bought} = #{old_total_bottles_bought} + #{nbr_bottles_returned}"
 
-    old_total_caps = total_caps
-
-    puts "nbr_bottles_returned #{old_nbr_bottles_returned} | additional caps from bottles returned #{old_nbr_bottles_returned} | total_caps #{old_total_caps} | nbr bottles redeemed from bottles #{nbr_bottles_redeemed_from_bottles} | nbr bottles redeemed from caps #{nbr_bottles_redeemed_from_caps} | remaining caps after caps were redeemed #{remaining_caps} | total nbr full bottles from recycling #{nbr_bottles_returned}"
-
-    # puts "nbr_bottles_returned #{old_nbr_bottles_returned} "
-    # puts "additional caps from bottles returned #{old_nbr_bottles_returned}"
-    # puts "nbr bottles earned from bottles #{nbr_bottles_returned_from_bottles}"
-    # puts "nbr bottles earned from caps #{nbr_bottles_returned_from_caps}"
-
-    # puts "remaining caps after caps were redeemed #{remainder_caps}"
-    # puts "total_caps #{total_caps}"
-    # puts "nbr full bottles from recycling #{nbr_bottles_returned}"
-
-
+    puts ""
+    puts "======================================================================="
+    puts ""
   end
 
+  puts ""
+  puts "remaining_bottles #{remaining_bottles}"
+  puts "remaining_caps #{remaining_caps}"
   puts "total bottles bought #{total_bottles_bought}"
 end
 
-iterative_total_bottles(10)
+# iterative_total_bottles(10)
 
 # ===============================================================
 
@@ -105,19 +172,13 @@ def repl
 
   while !quit do
 
-    puts "Enter the dollar amount you will spend on pop: "
+    puts "Enter the dollar amount, rounded to the nearest dollar, you will spend on pop: "
 
     dollar_amount = gets.chomp
 
     if dollar_amount != 'quit' && dollar_amount.to_f >= 2.00
 
-
-      original_quantity_purchased = dollar_amount.to_f / 2.00
-      original_quantity_purchased.floor()
-
-      # total_bottles(original_quantity_purchased)
-      puts "original_quantity_purchased #{original_quantity_purchased.to_i} total bottles #{total_bottles(original_quantity_purchased).to_i}"
-
+      iterative_total_bottles(dollar_amount.to_f/2.0)
 
     end
 
